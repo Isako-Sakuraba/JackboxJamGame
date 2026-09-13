2b5ee2b40a9c6641b31efc6a1504be20949a44ac
Isako <isako.workmail@gmail.com>
2026-09-13 14:33:15 +0300

    Add Player display names above head

---
+++ Assets/_Project/Scripts/Player/PlayerName.cs
@@ -0,0 +1,49 @@
+using PurrNet;
+using PurrNet.Lobby;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Player 
+{
+    public class PlayerName : NetworkIdentity
+    {
+        [SerializeField] private TMP_Text _nameText;
+
+        private string _displayName;
+
+        protected override void OnSpawned()
+        {
+            if (!isOwner)
+                return;
+
+            string displayName = localPlayer.ToString();
+
+            if (GameOrchestrator.active != null 
+                && GameOrchestrator.active.sessionProvider != null)
+            {
+                displayName = GameOrchestrator.active.sessionProvider.playerName;
+            }
+
+            SetDisplayName(displayName);
+        }
+
+        [ServerRpc]
+        private void SetDisplayName(string name)
+        {
+            _displayName = name;
+            SetDisplayNameOnClients(_displayName);
+        }
+
+        [ObserversRpc]
+        private void SetDisplayNameOnClients(string name)
+        {
+            _displayName = name;
+            UpdateDisplayName();
+        }
+
+        private void UpdateDisplayName()
+        {
+            _nameText.text = _displayName;
+        }
+    }
+}
